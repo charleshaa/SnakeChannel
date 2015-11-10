50c46189de75debc9361ac4319044405bb8a5ffc
Charles Haarman <carlos@Charless-MacBook-Pro.local>
2015-11-10 13:28:03 +0100

    Simple serveur/client qui envoie et recoit des données via une socket UDP

---
+++ client.py
@@ -0,0 +1,15 @@
+import socket
+import time
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 3100
+MESSAGE = "Hello, World!"
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+while True:
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    time.sleep(5)
