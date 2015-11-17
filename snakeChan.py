ac4cbb26b8498d0ba1c3b3254445cbfb8d2df9ba
Balthazar de Moncuit de Boiscuillé <balthazar.de.moncuit@gmail.com>
2015-11-17 14:14:55 +0100

    ++

--- snakeChan.py
+++ snakeChan.py
@@ -22,16 +22,12 @@ class SnakeChan:
 		return -1
 
 	def send(self, data, addr=0):
-		if addr == 0:
-			com = self.connections[0]
-		else:
-			com = self.connections[self.clientsIndex(addr)]
+		com = self.connections[self.clientsIndex(addr)]
 		if (com.isConnected):
 			self.socket.sendto(pack('I', com.req) + data, com.addr)
 			com.req += 1
 		else:
 			self.socket.sendto(pack('I', 0xFFFFFFFF) + data, addr)
-		data = None
 
 	def receive(self, time=0):
 		if time == 0:
@@ -42,7 +38,7 @@ class SnakeChan:
 			data, addr = self.socket.recvfrom(1024) #On recupere les data
 
 			if self.clientsIndex(addr)<0:
-				self.connections.append(dataConnection(addr))
+				self.connections.append(dataConnection(addr, 0))
 
 			com = self.connections[self.clientsIndex(addr)]
 			bytes = unpack('I', data[0:4])
@@ -57,44 +53,45 @@ class SnakeChan:
 						return (data, com.addr)
 			else:
 				if bytes == 0xFFFFFFFF:
-					if self.port != 0:
-						print data, com.etat
-						if com.etat == 0:
-							data = data.split(" ") #On split les data
-							if data[0] == "GetToken" and data[2] == "Snake":#On verifie la logique protocolaire
-								com.tokenA = data[1]
-								com.etat=1
-								com.tokenB = randint(0, 0xFFFFFFFF)#On genere un token B
-								self.send("Token " + str(com.tokenB)+" "+str(com.tokenA)+" "+str(self.Pnum), com.addr)#On envoi ce token
-						elif com.etat == 1:
-							data = data.split(" ") #On split les data
-							if data[0] == "Connect":
-								data = data[1].split("/")#Formate les data
-								del data[0]#idem
-								if data[1] == str(com.tokenB) and data[3] == str(self.Pnum):#Si la reponse correspond a la logique protocolaire...
-									print "ok"
-									self.send("Connected " + str(com.tokenB), com.addr)#Envoi la confirmation de connection au client
-									com.etat = 2#Passe l'etat du client a connecte
-									com.isConnected = True
-									com.compteur = 0
-							else:
-								if com.compteur >2:
-									com.etat = 0#Si le message n'est pas ce qu'on attend, le client regresse dans la machine d'etat
-									com.compteur = 0
-								else:
-									com.compteur += 1
-						elif com.etat == 2:
-							if com.compteur > 2:
-								com.etat = 0
-								com.isConnected =False
-							else:
-								com.compteur += 1
+					if com.type == 0:#Si type = client, on communique avec un client, on va prendre le role de serveur pour accepter la connection
+						self.acceptConnection(com, data)
 					else:
 						return data
 		return None
 
+	def acceptConnection(self, com, data):
+		if com.etat == 0:
+			data = data.split(" ") #On split les data
+			if data[0] == "GetToken" and data[2] == "Snake":#On verifie la logique protocolaire
+				com.tokenA = data[1]
+				com.etat=1
+				com.tokenB = randint(0, 0xFFFFFFFF)#On genere un token B
+				self.send("Token " + str(com.tokenB)+" "+str(com.tokenA)+" "+str(self.Pnum), com.addr)#On envoi ce token
+		elif com.etat == 1:
+			data = data.split(" ") #On split les data
+			if data[0] == "Connect":
+				data = data[1].split("/")#Formate les data
+				del data[0]#idem
+				if data[1] == str(com.tokenB) and data[3] == str(self.Pnum):#Si la reponse correspond a la logique protocolaire...
+					self.send("Connected " + str(com.tokenB), com.addr)#Envoi la confirmation de connection au client
+					com.etat = 2#Passe l'etat du client a connecte
+					com.isConnected = True
+					com.compteur = 0
+			else:
+				if com.compteur >2:
+					com.etat = 0#Si le message n'est pas ce qu'on attend, le client regresse dans la machine d'etat
+					com.compteur = 0
+				else:
+					com.compteur += 1
+		elif com.etat == 2:
+			if com.compteur > 2:
+				com.etat = 0
+				com.isConnected =False
+			else:
+				com.compteur += 1
+
 	def connect(self, addr):
-		self.connections.append(dataConnection(addr))
+		self.connections.append(dataConnection(addr, 1))
 		com = self.connections[self.clientsIndex(addr)]
 		com.pendingRequest = False
 		while (com.isConnected == False):
@@ -103,7 +100,6 @@ class SnakeChan:
 				self.send("GetToken " + str(com.tokenA) + " Snake", com.addr)#Envoi le premier token
 				data = self.receive(1)
 				if data != None:
-					print "client reciot: "+data
 					data = data.split(" ", 4) #On split les data
 					if data[2] == str(com.tokenA): #On verifie que la logique protocolaire ai ete respecte
 						com.tokenB = data[1] #On recupere le token B
@@ -132,10 +128,8 @@ class SnakeChan:
 			   else:
 			   	com.compteur += 1
 
-						   
-
 class dataConnection: #Contient les donnes pour chaque client
-	def __init__(self, addr):
+	def __init__(self, addr, type):
 		self.addr = addr
 		self.etat = 0
 		self.tokenA = None
@@ -145,3 +139,4 @@ class dataConnection: #Contient les donnes pour chaque client
 		self.compteur = 0
 		self.req = 1
 		self.ind = 0
+		self.type = type #type 0 = client, type 1 = serveur
