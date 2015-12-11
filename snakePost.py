07fc0620ee137cbd7652fa2732910be14c66c20f
Balthazar de Moncuit <balthazar.de.moncuit@gmail.com>
2015-12-11 10:36:26 +0100

    Sometimes, there is a None addr passed to send()
    Patch it by checking id addr != None in send()... but it is an hot fix. It should not occur.

--- snakePost.py
+++ snakePost.py
@@ -12,21 +12,23 @@ from snakeChan import SnakeChan
 
 class SnakePost:
 
-	def __init__(self, canalTransmission):
+	def __init__(self, canalTransmission, ident):
 		pygame.init()
 		pygame.time.set_timer(USEREVENT+1, 30)
 		self.canalTransmission = canalTransmission
 		self.commDict = {}
-		self.ackDict = {}
+		self.pendingAck = (None, None)
+		self.id = ident
 		print color.GREEN + "snakePost instanciated." + color.END
 
-	def send(self, data, addr, seq=0, ack=0):
-		AckToSendToAddr = self.ackDict.get(addr) #Recupere la liste des ack a faire pour cette addresse
-		if AckToSendToAddr: #Si il y au moins un ack a faire
-			ack = AckToSendToAddr.pop() #On fait du piggybagging
+	def send(self, data, addr, seq=0):
+		ack=0
+		if self.pendingAck[0] == addr:
+			ack = self.pendingAck[1]
+			self.pendingAck = (None, None)
 		print self.id, "envoi", seq, ack, data
-		self.canalTransmission.sendto( pack( 'I', ((ack<<16)|seq) ) + data, addr )
-
+		if addr != None:
+			self.canalTransmission.sendto( pack( 'I', ((ack<<16)|seq) ) + data, addr )
 
 	def sendSecure(self, data, addr):#N'envoi pas, va simplement mettre en attente le message.
 		seq = random.randint(1, 0xFFFF)
@@ -40,53 +42,49 @@ class SnakePost:
 		for addr, dataToSendToAddr in self.commDict.iteritems():
 			if dataToSendToAddr:
 				data = dataToSendToAddr[0]
-				self.send(data[0], addr, data[1], 0)
-
-	def sendAllPendingAck(self):
-		for addr, listAck in self.ackDict.iteritems():
-			while listAck:
-				ack = listAck.pop()
-				self.send( "", addr, 0, ack )
+				self.send(data[0], addr, data[1])
 
 	def listen(self):
 		data = None
 		addr = None
 
-		#Pour avancer, il faut recevoir un message qui ne contienne pas qu'un ack
-		while data == None:
-			##TOUTES LES 30[ms] ON ENVOI LES MESSAGES SECURISE ET ON ACK LES ACK NON PIGGYBAGGE##
-			for event in pygame.event.get():
-				if event.type == USEREVENT+1:
-					self.sendAllSecureQueued() #calling the function wheever we get timer event.
-					self.sendAllPendingAck()
+		##TOUTES LES 30[ms] ON ENVOI LES MESSAGES SECURISE##
+		for event in pygame.event.get():
+			if event.type == USEREVENT+1:
+				self.sendAllSecureQueued() #calling the function wheever we get timer event.
+		
+		if self.pendingAck[1] != None: #Si on a encore un ack a faire, on le fait
+			self.send("", self.pendingAck[0])
 
-			data, addr = self.canalTransmission.receive( 0 ) #non bloquant
-			
-			if(data != None):
-				##ON PREPARE AU TRAITEMENT LES DONNEES RECU##
-				bytes = unpack( 'I', data[0:4] )
-				bytes = bytes[0]
-				data = data[4:]
-				if data == "":
-					data = None
-				bytesT = bytes
-				ack = ( (bytes&0xFFFF0000) >> 16 ) #Numero de sequence qu'on va devoir ack
-				seq = bytesT&0x0000FFFF #Numero d'ack qui confirme un envoi securise
-				print self.id, "recoi", seq, ack, data
+		data, addr = self.canalTransmission.receive( 0 ) #non bloquant
+		if data == "": ####!!!! We have to check if snakChan return None or "" if non data is received
+				data = None
+		if(data != None):
+			##ON PREPARE AU TRAITEMENT LES DONNEES RECU##
+			bytes = unpack( 'I', data[0:4] )
+			bytes = bytes[0]
+			data = data[4:]
+			if data == "": ####!!!! We have to check if snakChan return None or "" if non data is received
+				data = None
+			bytesT = bytes
+			ack = ( (bytes&0xFFFF0000) >> 16 ) #Numero d'ack qui confirme un envoi securise
+			seq = bytesT&0x0000FFFF #Numero de sequence qu'on va devoir ack
+			print self.id, "recoi", seq, ack, data
 
-				##TRAITEMENT DU ACK SI IL EST PLUS GRAND QUE ZERO##
-				if ( ack>0 ):
-					dataToSendToAddr = self.commDict.get( addr )#Recupere la liste de donne a envoyer a addr
-					if dataToSendToAddr:
-						dataToSendToAddr = [(dat, sequence) for dat, sequence in dataToSendToAddr if sequence != ack]#Retourne la liste moins l'element confirme
-						self.commDict[addr] = dataToSendToAddr
+			##TRAITEMENT DU ACK SI IL EST PLUS GRAND QUE ZERO##
+			if ( ack>0 ):
+				dataToSendToAddr = self.commDict.get( addr )#Recupere la liste de donne a envoyer a addr
+				if dataToSendToAddr:
+					dataToSendToAddr = [(dat, sequence) for dat, sequence in dataToSendToAddr if sequence != ack]#Retourne la liste moins l'element confirme
+					self.commDict[addr] = dataToSendToAddr
 
-				##TRAITEMENT DE LA SEQUENCE SI ELLE EST PLUS GRAND QUE ZERO##
-				if ( seq>0 ):
-					AckToSendToAddr = self.ackDict.get( addr )
-					if AckToSendToAddr != None:
-						AckToSendToAddr.append( seq )
-					else:
-						self.ackDict[addr] = [seq]
+			##TRAITEMENT DE LA SEQUENCE SI ELLE EST PLUS GRAND QUE ZERO##
+			if ( seq>0 ):
+				self.pendingAck = (addr, seq)
+				dataToSendToAddr = self.commDict.get(addr)
+				if dataToSendToAddr:
+					print "ICI"
+					data = dataToSendToAddr[0]
+					self.send(data[0], addr, data[1])
 
 		return ( data, addr )
