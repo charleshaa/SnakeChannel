264f42b85cf8530831bb64a91517d76e34fa2067
Balthazar de Moncuit <balthazar.de.moncuit@gmail.com>
2016-01-05 15:36:50 +0100

    Delete gameServer.py

--- gameServer.py
+++
@@ -1,9 +0,0 @@
-import time
-from snakeChan import SnakeChan
-from snakePost import SnakePost
-
-serveur = SnakePost(SnakeChan('127.0.0.1', 3100))
-while 1:
-	data = serveur.listen()
-	if data[0] == "ping":
-		serveur.sendSecure("ok", data[1])
